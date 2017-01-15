57b73b854b64a3e73bc91c5fb4bc8237fd236176
Alexander Sopov <sopovalexander@gmail.com>
2017-01-15 16:55:51 +0100

    added project!

---
+++ Assets/VRND-Course2-Project/Scripts/TriggerAnimation.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TriggerAnimation : MonoBehaviour {
+    public string AnimationName;
+    public Animator stateMachine;
+    private GvrViewer viewer;
+
+
+    void Start() {
+        // Locate the GvrViewer instance
+        viewer = (GvrViewer)FindObjectOfType(typeof(GvrViewer));
+        if (viewer == null)
+        {
+            Debug.LogError("No GvrViewer found. Please drag the GvrViewerMain prefab into the scene.");
+            return;
+        }
+    }
+
+    void Update() {
+        viewer.UpdateState(); //need to update the data here otherwise we dont get mouse clicks; this is because we are automatically creating the GVRSDK (seems like a bug)
+        if (viewer.Triggered)
+        {
+            Debug.LogError("whatevs");
+            stateMachine.SetTrigger(AnimationName);
+        }
+        
+    }
+
+}
